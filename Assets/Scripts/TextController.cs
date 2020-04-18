0bbde1822764a678e0e548b0f08fea700d3c7f05
FinisTP <53522152+FinisTP@users.noreply.github.com>
2020-04-19 01:59:10 +0700

    Make monologues

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextController : MonoBehaviour
+{
+    public Text currentText;
+    public float textSpeed = 0.5f;
+    public Button continueButton;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        continueButton.gameObject.SetActive(false);
+        sentences = new Queue<string>();
+    }
+
+    public void StartText(TextBehaviour text) 
+    {
+        sentences.Clear();
+
+        foreach(string sentence in text.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        continueButton.gameObject.SetActive(false);
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        } else if (sentences.Count == 1)
+        {
+            currentText.alignment = TextAnchor.MiddleCenter;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        currentText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            currentText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        continueButton.gameObject.SetActive(true);
+    }
+
+    void EndDialogue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour
+{
+    public TextBehaviour text;
+    
+    void Start()
+    {
+        StartCoroutine(WaitForLoading());
+    }
+
+    IEnumerator WaitForLoading()
+    {
+        yield return new WaitForSeconds(1f);
+        TriggerText();
+    }
+    public void TriggerText()
+    {
+        FindObjectOfType<TextController>().StartText(text);
+    }
+}
