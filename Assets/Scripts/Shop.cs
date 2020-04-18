3e8f6295c9a0be0f3ffa65db52104b91deec190b
Bacon <nguyenlamtuong2001@gmail.com>
2020-04-18 23:32:13 +0700

    Store update

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject dark;
+    public GameObject shop;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            dark.SetActive(true);
+            shop.SetActive(true);
+        }        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            dark.SetActive(false);
+            shop.SetActive(false);
+        }
+    }
+}
